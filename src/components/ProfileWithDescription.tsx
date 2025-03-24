f056191a0ff896ea4d315b9ef4f7c77bca3fbe83
Chottcodes <cwellshott@gmail.com>
2025-03-23 23:27:38 -0700

    adding semantics

--- src/components/ProfileWithDescription.tsx
+++ src/components/ProfileWithDescription.tsx
@@ -8,20 +8,20 @@ interface propTypes {
 const ProfileWithDescription = (props:propTypes) => {
   const {Name,Username,Location} = props
   return (
-    <div className='w-full h-full flex flex-col justify-center items-center gap-2 text-2xl text-white'>
-      <div className=" h-[125px] w-[125px] bg-green-800 flex justify-center items-center rounded-full overflow-hidden">
+    <section className='w-full h-full flex flex-col justify-center items-center gap-2 text-2xl text-white'>
+      <header className=" h-[125px] w-[125px] bg-green-800 flex justify-center items-center rounded-full overflow-hidden">
       <Image className='w-full h-full' src={"/assets/images/motorcycle-tires.jpg"} width={100} height={100} alt='motorcycle tires' />
-      </div>
+      </header>
       <h1>{Name}</h1>
-      <div className="w-[35%] h-[10%] text-lg flex justify-center items-center gap-2">
+      <figure className="w-[35%] h-[10%] text-lg flex justify-center items-center gap-2">
         <Image className='h-[20px] w-[20px]' src={"/assets/images/user.png"} width={100} height={100} alt='Icon'/>
-        <p>{`@${Username}`}</p>
-      </div>
-      <div className="w-[35%] h-[10%] text-lg flex justify-center items-center gap-2">
+        <figcaption>{`@${Username}`}</figcaption>
+      </figure>
+      <figure className="w-[35%] h-[10%] text-lg flex justify-center items-center gap-2">
       <Image className='h-[20px] w-[20px]' src={"/assets/images/location.png"} width={100} height={100} alt='Icon'/>
-      <p>{Location}</p>
-      </div>
-    </div>
+      <figcaption>{Location}</figcaption>
+      </figure>
+    </section>
   )
 }
 
