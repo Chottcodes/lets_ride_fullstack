276f3d1bade7b27e35950b72fcc3ab1d58b75144
Chottcodes <cwellshott@gmail.com>
2025-03-24 23:56:15 -0700

    update to routing

--- src/app/pages/Login/login-section/page.tsx
+++ src/app/pages/Login/login-section/page.tsx
@@ -1,32 +1,30 @@
-"use client"
-
-import BackButtonComponent from '@/components/BackButtonComponent';
-import PrimaryButton from '@/components/PrimaryButton'
-import { useRouter } from 'next/navigation';
-import React from 'react'
+"use client";
+import BackButtonComponent from "@/components/BackButtonComponent";
+import PrimaryButton from "@/components/PrimaryButton";
+import { useRouter } from "next/router";
+import React from "react";
 
 const LoginSection = () => {
-      const { push } = useRouter(); 
-    
+  
+  const { push } = useRouter();
+
   return (
     <div>
-    <BackButtonComponent onClick={() => push("/")}/>
-        
-<div className='flex flex-col justify-center items-center text-white mt-10'>
+      <BackButtonComponent onClick={() => push("/")} />
 
+      <div className="flex flex-col justify-center items-center text-white mt-10">
+        <div className="text-[46px] pb-8">
+          <h1>
+            Let's <span className="text-blue-600 tracking-widest">Ride</span>
+          </h1>
+        </div>
 
-<div className="text-[46px] pb-8">
-  <h1>
-    Let's <span className="text-blue-600 tracking-widest">Ride</span>
-  </h1>
-</div>
+        <div className="flex flex-col justify-center items-center">
+          <hr className="w-[20rem] pt-10" />
+          <h1 className="text-4xl tracking-widest mb-20">Login</h1>
+        </div>
 
-<div className='flex flex-col justify-center items-center'>
-<hr className="w-[20rem] pt-10" />
-<h1 className="text-4xl tracking-widest mb-20">Login</h1>
-</div>
-
-      <div className="w-100 flex flex-col gap-5">
+        <div className="w-100 flex flex-col gap-5">
           <div className="">
             <h2>Enter Email</h2>
           </div>
@@ -63,22 +61,47 @@ const LoginSection = () => {
               className="h-[20px] absolute left-[90%] cursor-pointer"
             />
           </div>
-          
-          <h1>forgot <button onClick={() => push("/pages/Login/resetpassword")} className="text-[#506FFD] hover:text-[#2e53fc] cursor-pointer"> password</button></h1>
 
-          <div className='flex flex-col justify-center items-center'>
-            <PrimaryButton buttonText="Log In" isBackgroundDark={true} onClick={() => push("/home/your-profile")}/>
-              <br /> 
-            <h1 className='inline-flex'> <span> <hr /> </span> Or <span><hr /></span></h1>
+          <h1>
+            forgot{" "}
+            <button
+              onClick={() => push("/")}
+              className="text-[#506FFD] hover:text-[#2e53fc] cursor-pointer"
+            >
+              {" "}
+              password
+            </button>
+          </h1>
+
+          <div className="flex flex-col justify-center items-center">
+            <PrimaryButton
+              buttonText="Log In"
+              isBackgroundDark={true}
+              onClick={() => push("/home/your-profile")}
+            />
             <br />
-          <PrimaryButton buttonText="Back To Sign In" isBackgroundDark={false} onClick={() => push("/home/your-profile")}/>
+            <h1 className="inline-flex">
+              {" "}
+              <span>
+                {" "}
+                <hr />{" "}
+              </span>{" "}
+              Or{" "}
+              <span>
+                <hr />
+              </span>
+            </h1>
+            <br />
+            <PrimaryButton
+              buttonText="Back To Sign In"
+              isBackgroundDark={false}
+              onClick={() => push("/home/your-profile")}
+            />
           </div>
-           
-
+        </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default LoginSection
+export default LoginSection;
--- src/components/DesktopNavBar.tsx
+++ src/components/DesktopNavBar.tsx
@@ -1,11 +1,86 @@
-import React from 'react'
-
-const DesktopNavBar = () => {
-  return (
-    <nav className='w-full h-full'>
-        
-    </nav>
-  )
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+interface propTypes {
+  isHomeOn: boolean;
+  isLocationOn: boolean;
+  isGalleryOn: boolean;
+  isProfileOn: boolean;
 }
+const DesktopNavBar = (prop: propTypes) => {
+  const { isHomeOn, isLocationOn, isGalleryOn, isProfileOn } = prop;
+  const handleHomeButton = () => {
+    //Redirect to home
+  };
+  const handleLocationButton = () => {
+    //Redirect to Location page
+  };
+  const handleGalleryButton = () => {
+    //reditrect to Gallery Page
+  };
+  const handleProfileButton = () => {
+    //redirect to profile page
+  };
 
-export default DesktopNavBar
+  return (
+    <nav className="w-full h-full hidden lg:flex lg:flex-col  justify-evenly items-center">
+      <section className="h-[50%] w-full flex flex-col justify-evenly items-center">
+      <button onClick={handleHomeButton} className="cursor-pointer">
+        <Image
+          className="h-[35px] w-[35px] lg:h-[25px] lg:w-[25px]"
+          src={
+            isHomeOn
+              ? "/assets/images/home(2).png"
+              : "/assets/images/home(1).png"
+          }
+          width={100}
+          height={100}
+          alt="Home Icon"
+        />
+      </button>
+      <button onClick={handleLocationButton} className="cursor-pointer">
+        <Image
+          className="h-[35px] w-[35px] lg:h-[25px] lg:w-[25px]"
+          src={
+            isLocationOn
+              ? "/assets/images/location(1).png"
+              : "/assets/images/location.png"
+          }
+          width={100}
+          height={100}
+          alt="Location Icon"
+        />
+      </button>
+      <button onClick={handleGalleryButton} className="cursor-pointer">
+        <Image
+          className="h-[35px] w-[35px] lg:h-[25px] lg:w-[25px]"
+          src={
+            isGalleryOn
+              ? "/assets/images/gallery(1).png"
+              : "/assets/images/gallery.png"
+          }
+          width={100}
+          height={100}
+          alt="Gallery Icon"
+        />
+      </button>
+      </section>
+      <section className="w-full h-[50%] flex justify-center items-end">
+      <button
+        onClick={handleProfileButton}
+        className={`${
+          isProfileOn ? "border-2 border-blue-700" : "border-none"
+        } rounded-full overflow-hidden mb-32 cursor-pointer`}
+      >
+        <Avatar className="w-[55px] h-full lg:h-[30px] lg:w-[30px]">
+          <AvatarImage src="/assets/images/motorcycle-tires.jpg" />
+          <AvatarFallback>Profile Picture</AvatarFallback>
+        </Avatar>
+      </button>
+      </section>
+    </nav>
+  );
+};
+
+export default DesktopNavBar;
