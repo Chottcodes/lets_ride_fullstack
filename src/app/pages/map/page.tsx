947030ab694546276f14200f943791c03f95d121
Chottcodes <cwellshott@gmail.com>
2025-04-15 03:42:14 -0700

    update to map

---
+++ src/app/pages/map/page.tsx
@@ -0,0 +1,23 @@
+import MapDisplay from '@/components/mapDisplay'
+import MobileNavBar from '@/components/navbars/MobileNavBar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen relative'>
+        <header className='bg-blue-700/2 backdrop-blur-2xl absolute top-0 w-full h-[10%] flex justify-center items-center z-10' >
+            <button className='cursor pointer text-white'>Community routes</button>
+        </header>
+        <main className='h-full w-full'>
+            <section className='h-full w-full'>
+                <MapDisplay/>
+            </section>
+        </main>
+        <footer className='absolute bottom-0 w-full h-[10%] bg-black/70 backdrop-blur-2xl lg:hidden flex items-center'>
+            <MobileNavBar isHomeOn={false} isGalleryOn={false} isLocationOn={true} isProfileOn={false} />
+        </footer>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/mapDisplay.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React, { useState } from "react";
+import { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+
+const MapDisplay = () => {
+  const [location, setLocation] = useState<{ lat: number; lng: number }>({
+    lat: 0,
+    lng: 0,
+  });
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+  const mapzoom: number = 12;
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      alert("browser not supported");
+      setLocation({ lat: 37.7749, lng: -122.4194 });
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      },
+      (error) => {
+        console.warn(error);
+        setLocation({ lat: 37.7749, lng: -122.4194 });
+      },
+      {
+        enableHighAccuracy: true,
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (!location || !mapContainerRef.current) return;
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/chott1/cm82q157o00aq01sjhffp707j",
+      center: [location.lng, location.lat],
+      zoom: mapzoom,
+    });
+    new mapboxgl.Marker().setLngLat([location.lng, location.lat]).addTo(map);
+    return () => map.remove();
+  }, [location]);
+
+  return (
+    <div className="w-full h-full relative text-white">
+      <div className="w-full h-full" ref={mapContainerRef} />
+      <div className="absolute bottom-0 bg-blue-900 w-full h-[10%]"/>
+    </div>
+  );
+};
+
+export default MapDisplay;
