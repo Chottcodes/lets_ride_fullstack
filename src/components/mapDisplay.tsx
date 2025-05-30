a4e36428a0d0a427a5fadb895a702a0a5caed633
Chottcodes <cwellshott@gmail.com>
2025-05-29 18:39:33 -0700

    fix to changes

--- src/components/mapDisplay.tsx
+++ src/components/mapDisplay.tsx
@@ -504,7 +504,7 @@ const MapDisplay = () => {
                     />
                   </div>
                   <div className="text-xs text-gray-400">
-                    Please don't close this window while uploading
+                    Please dont close this window while uploading
                   </div>
                 </div>
               )}
